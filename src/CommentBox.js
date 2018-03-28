ccdbe734d796b2c383cfc8583eb25f891346d412
Jeremiah Pratt <pratt@cooper.edu>
2018-03-27 20:21:13 -0400

    add reference files for databasing

---
+++ src/CommentBox.js
@@ -0,0 +1,48 @@
+//CommentBox.js
+import React, { Component } from 'react';
+import axios from 'axios';
+import CommentList from './CommentList';
+import CommentForm from './CommentForm';
+import style from './style';
+
+class CommentBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { data: [] }
+        this.loadCommentsFromServer = this.loadCommentsFromServer.bind(this);
+        this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+    }
+    _myHandler(props) {
+        console.log(props);
+    }
+    loadCommentsFromServer() {
+        axios.get(this.props.url)
+            .then(res => {
+                this.setState({ data: res.data});
+            })
+    }
+    handleCommentSubmit(comment) {
+        axios.post(this.props.url, comment)
+            .then(res => {
+                  this.setState({ data: res });
+            })
+            .catch(err => {
+                  console.error(err);
+            });
+    }
+    componentDidMout() {
+        this.loadCommentsFromServer();
+        setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+    }
+    render() {
+        return (
+                <div style={ style.commentBox }>
+                <h2>Comments:</h2>
+                <CommentList data={ this.state.data }/>
+                <CommentForm onCommentSubmit={this.handleCommentSubmit }/>
+                </div>
+               )
+    }
+}
+
+export default CommentBox;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+//index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentBox from './CommentBox';
+
+ReactDOM.render(
+        <CommentBox
+        url='http://localhost:3001/api/comments'
+        pollInterval={2000} />,
+        document.getElementById('root')
+    );
